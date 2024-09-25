9923ce5c61ec186c325cd33f93a74633f14d988e
abdyrakhman <medzholdoshbaev@gmail.com>
2024-09-25 19:21:04 +0600

    added SecurityConfig

---
+++ src/main/java/dzholdoshbaev/laboratory/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package dzholdoshbaev.laboratory.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .formLogin(form -> form
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/auth/login")
+                        .successHandler(new CustomAuthenticationSuccessHandler())
+                        .failureUrl("/auth/login?error=true")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/auth/logout")
+                        .logoutSuccessUrl("/auth/login")
+                        .permitAll())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(HttpMethod.POST,"/images").authenticated()
+                        .requestMatchers(HttpMethod.GET,"/profile").authenticated()
+                        .requestMatchers(HttpMethod.GET,"/profile/edit").authenticated()
+                        .anyRequest().permitAll());
+        return http.build();
+    }
+
+
+
+
+}
